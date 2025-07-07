b19c1bf5e95e90a0f0107a1d2a72a3a6dd4a113a
greedwon <gn123321@mailbox.org>
2025-07-06 17:50:20 -0700

    more progress on report

--- main.py
+++ main.py
@@ -5,13 +5,24 @@ def get_book_text(file):
         book_text = f.read()
         return book_text
 
+def sort_on(items):
+    return items["num"]
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book = "books/frankenstein.txt"
+    book_text = get_book_text(book)
     num_words = get_word_count(book_text)
-    letter_tally = get_character_count(book_text)
-    print(f"{num_words} words found in the document")
-    print(letter_tally)
+    char_count = get_character_count(book_text)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    print(f"{char_count}")
+    for char in char_count:
+        char = char_count["char"]
+        num = char_count["num"]
+        print(f"{char}: {num}")
     return
 
 main()
--- stats.py
+++ stats.py
@@ -6,6 +6,7 @@ def get_word_count(text):
 def get_character_count(text):
     words = text.split()
     letter_count = {}
+    report_list = []
     for word in words:
         for letter in word:
             lower_letter = letter.lower()
@@ -13,4 +14,10 @@ def get_character_count(text):
                 letter_count[lower_letter] = 1
             else:
                 letter_count[lower_letter] += 1
+    for letter in letter_count:
+        letter_stats = {}
+
     return letter_count
+
+def sort_key(letters):
+    return letters
